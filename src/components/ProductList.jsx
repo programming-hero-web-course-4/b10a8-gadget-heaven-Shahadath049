3ca6547033157c6121b791ed585331b12f0e12e8
Shahadath049 <zamirshahadath049@gmail.com>
2025-02-13 20:01:30 +0600

    sidebar added

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,25 +2,38 @@ import { useEffect, useState } from "react";
 import Product from "./Product";
 
 const ProductList = () => {
-  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
+    
 
   useEffect(() => {
     fetch("/Products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+    
+    const handleFilter = (category) => {
+        if (category === "All Products") {
+            setProducts(products);
+        } else {
+            const filteredProducts = products.filter(
+                (product) => product.category === category
+            );
+            setProducts(filteredProducts);
+        }
+    };
 return (
     <div>
         <h1 className="text-5xl font-bold text-center mt-32 mb-20">Explore Cutting-Edge Gadgets</h1>
         <div className="container mx-auto p-4 flex items-start">
             <div className="flex flex-col space-y-4 w-1/4">
-                <button className="text-purple-600 border border-purple-600 rounded-full px-4 py-2 text-sm hover:bg-purple-50 transition-colors">
+                <button onClick={() => handleFilter("All Products")} className="text-purple-600 border border-purple-600 rounded-full px-4 py-2 text-sm hover:bg-purple-50 transition-colors">
                     All Products
                 </button>
                 {[...new Set(products.map((product) => product.category))].map(
                     (category, index) => (
-                        <button key={index} className="text-purple-600 border border-purple-600 rounded-full px-4 py-2 text-sm hover:bg-purple-50 transition-colors">
+                        <button onClick={() => handleFilter(category)} key={index} className="text-purple-600 border border-purple-600 rounded-full px-4 py-2 text-sm hover:bg-purple-50 transition-colors">
                             {category}
+                            
                         </button>
                     )
                 )}
